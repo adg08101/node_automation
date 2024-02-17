c04ac92862421e529bb9b397ab9534e1a0315c93
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2024-02-16 22:44:55 -0300

    docker commit

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-import express from 'express';
-import { urlencoded, json } from 'body-parser';
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require ('mongoose');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-// Converts an incoming request (POST-GET...) to a JSON object
-app.use(urlencoded({ extended: false }));
-app.use(json());
+const animalSchema = mongoose.model('animal', new mongoose.Schema({
+	type: String,
+	status: String
+}))
 
-app.get('/', function (req, res) {
+// Converts an incoming request (POST-GET...) to a JSON object
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://adg08101:adg08101@moon:27017/animals?authSource=admin')
+
+app.get('/', async (_req, res) => {
+	const animals = await animalSchema.find();
+	console.log('listing...')
 	res.status(200).send({
-		message: 'GET Home route working fine!'
+		message: `listing ${animals}...`
+	});
+});
+
+app.get('/create', async (_req, res) => {
+	await animalSchema.create({ type: 'Pig', status: 'Happy' })
+	console.log('creating...')
+	res.status(200).send({
+		message: `creating animal... ok`
 	});
 });
 
